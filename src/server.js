f669a3958c9a106f4c7ba0a7071738bb73cddcb8
rusketh <rusketh@gmail.com>
2026-02-18 18:49:56 +0000

    Added more security to the web server.

--- src/server.js
+++ src/server.js
@@ -79,6 +79,18 @@ const cookieOptions = {
 };
 
 /******************************************************************************************
+ * Security
+ */
+
+WebApp.use((req, res, next) => {
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-Frame-Options', 'DENY');
+    res.setHeader('Content-Security-Policy', "default-src 'self'; img-src 'self' data: https://*.telegram.org; video-src 'self' https://*.telegram.org;");
+    next();
+});
+
+
+/******************************************************************************************
  * Middleware
  */
 
